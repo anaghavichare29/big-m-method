850bf9485c69a671ba8a27ed96620bf60c0b3583
ankita01209 <ankitashanbhag25@gmail.com>
2026-03-29 19:51:48 +0530

    Add parse_objective function to parse objective strings

---
+++ parts/ankita.py
@@ -0,0 +1,15 @@
+def parse_objective(obj_str):
+    terms = re.findall(r'([+-]?\d*)x(\d+)', obj_str.replace(" ", ""))
+    max_var = max(int(var) for _, var in terms)
+
+    c = [0] * max_var
+    for coef, var in terms:
+        if coef in ["", "+"]:
+            coef = 1
+        elif coef == "-":
+            coef = -1
+        else:
+            coef = int(coef)
+        c[int(var) - 1] = coef
+
+    return c
