40e589d5ad8385d8ce5b18d3598fc386be1d601d
ankita01209 <ankitashanbhag25@gmail.com>
2026-03-29 19:53:33 +0530

    Limit parse_objective to 3 variables
    Added a limit to the number of variables supported in the parse_objective function.

--- parts/ankita.py
+++ parts/ankita.py
@@ -13,3 +13,23 @@ def parse_objective(obj_str):
         c[int(var) - 1] = coef
 
     return c
+
+def parse_objective(obj_str):
+    terms = re.findall(r'([+-]?\d*)x(\d+)', obj_str.replace(" ", ""))
+    max_var = max(int(var) for _, var in terms)
+
+    # 🔴 LIMIT TO 3 VARIABLES
+    if max_var > 3:
+        raise ValueError("Only up to 3 variables are supported.")
+
+    c = [0] * max_var
+    for coef, var in terms:
+        if coef in ["", "+"]:
+            coef = 1
+        elif coef == "-":
+            coef = -1
+        else:
+            coef = int(coef)
+        c[int(var) - 1] = coef
+
+    return c
